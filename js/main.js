d73364fb77740bdf3b021aa0b26ef0ed8317ac63
manimurugiah <manikandanmpm6@gmail.com>
2022-12-20 10:34:19 +0530

    add a new file

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let pname=parseInt(prompt("Enter tha index of an array"));
+const names=["mani","ajith","muthu","murugan"];
+names[pname]=prompt("Enter the changeable item");
+document.write(names+"<br>");
